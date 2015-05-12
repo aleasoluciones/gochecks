7a4616c4a4fbf6a2f2dedde5ff509c9aea1452ed
Eduardo Ferro <eduardo.ferro.aldama@gmail.com>
2015-05-12 12:24:42 +0200

    Added a minimal use example

---
+++ example/example.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"time"
+
+	"github.com/aleasoluciones/felixcheck"
+)
+
+func main() {
+	checkEngine := felixcheck.NewCheckEngine(felixcheck.NewRiemannPublisher("127.0.0.1:5555"))
+	period := 5 * time.Second
+	checkEngine.AddCheck(
+		felixcheck.NewHttpChecker("golang", "http", "http://www.golang.org", 200).Tags("production", "web").Ttl(50),
+		period)
+
+	for {
+		time.Sleep(2 * time.Second)
+	}
+}

57355fdcd01b600b044a2be99e309da77df91f59
Eduardo Ferro Aldama <eduardo.ferro.aldama@gmail.com>
2015-07-24 14:56:37 +0200

    Added doc for jenkis job checker

--- jenkinschecks.go
+++ jenkinschecks.go
@@ -19,8 +19,10 @@ type JobsMessage struct {
 	Jobs []JobStatus `json:"jobs"`
 }
 
+// NewJenkinsJobsChecker returns a check function that validate that the jenkins api is accesible and all the jenkins
+// jobs matching the given jobRegExp are ok (blue color). When one or more of these jobs are in error the event is critical
+// and the jobs names are included at the event description
 func NewJenkinsJobsChecker(host, service, jenkinsBaseURL string, jobRegExp string) CheckFunction {
-
 	return func() Event {
 
 		brokenJobs := []string{}

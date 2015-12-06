e4cef816ae0c6113b5e022a98016267c73c1f8be
Eduardo Ferro Aldama <eduardo.ferro.aldama@gmail.com>
2015-12-06 22:26:21 +0100

    Added doc string

--- amqp2checkengine.go
+++ amqp2checkengine.go
@@ -5,7 +5,8 @@ import (
 	"github.com/aleasoluciones/simpleamqp"
 )
 
-func amqp2CheckEngine(checkEngine *CheckEngine, brokerURI, exchange, topic, queue string, queueOptions *simpleamqp.QueueOptions) {
+// Amqp2CheckEngine continously add check result from a queue  (generated using RabbitMqPublisher) to a given CheckEngine
+func Amqp2CheckEngine(checkEngine *CheckEngine, brokerURI, exchange, topic, queue string, queueOptions *simpleamqp.QueueOptions) {
 
 	if queueOptions == nil {
 		queueOptions = &simpleamqp.QueueOptions{Durable: false, Delete: true, Exclusive: true}

e5ea69d7841ae1abe730b6bda4338a969b32355e
Jaime Gil de Sagredo Luna <jgil@alea-soluciones.com>
2016-02-01 16:27:11 +0100

    Check for unresolved sentry issues

---
+++ sentrychecks.go
@@ -0,0 +1,31 @@
+package gochecks
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func NewSentryUnresolvedIssuesChecker(host, service, sentryBaseUrl, projectName string) CheckFunction {
+	return func() Event {
+		response, err := http.Get(sentryBaseUrl + "/api/0/projects/" + projectName + "/issues/?query=is:unresolved&statsPeriod=24h")
+		if err != nil {
+			return Event{Host: host, Service: service, State: "critical", Description: err.Error()}
+		}
+		defer response.Body.Close()
+		if response.StatusCode != http.StatusOk {
+			return Event{Host: host, Service: service, State: "critical", Description: fmt.Sprintf("Response %d", response.StatusCode)}
+		}
+		decoder := json.NewDecoder(response.Body)
+		var unresolvedIssues []interface{}
+		err = decoder.Decode(&unresolvedIssues)
+		if err != nil {
+			return Event{Host: host, Service: service, State: "critical", Description: err.Error()}
+		}
+		state := "ok"
+		if len(unresolvedIssues) != 0 {
+			state := "critical"
+		}
+		return Event{Host: host, Service: service, State: state, Metric: len(unresolvedIssues)}
+	}
+}

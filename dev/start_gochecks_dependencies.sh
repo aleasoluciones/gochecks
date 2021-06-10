939a0decbd08ce24fc480ef91aecd8d9122ffcb2
javikalsan <jcalzado@alea-soluciones.com>
2021-06-10 12:18:51 +0200

    [INTERNAL] Update postgresql version

--- dev/start_gochecks_dependencies.sh
+++ dev/start_gochecks_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker run --name mysql_gochecks -p 3306:3306 -e MYSQL_ROOT_PASSWORD=rootpass -d mysql:5.5
-docker run --name postgres_gochecks -p 5432:5432 -e POSTGRES_PASSWORD=mysecretpassword -d postgres:9.5
+docker run --name postgres_gochecks -p 5432:5432 -e POSTGRES_PASSWORD=mysecretpassword -d postgres:13.3
 docker run --name rabbitmq_gochecks -p 5672:5672 -d rabbitmq
